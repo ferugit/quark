adb07b5f4dde1f2aecf8b3cc30ca2f98f4ef0ce5
ferugit <wiliam.lopezgavilanez@telefonica.com>
2023-03-01 15:59:17 +0100

    add a sample bash script to train and evaluate

--- train_model.sh
+++ train_model.sh
@@ -1,3 +1,19 @@
-#!
+#!/bin/bash
+# Fernando López Gavilánez, 2023
+
+
+# Set training parameters
+optimizer='adam'
+momentum=0.9
+weight_decay=0.0
+seed=2023
+epochs=80
+batch_size=12
+patience=10
+lr=0.001
+
+# arch
+# augments
+# etc
 
 python -u src/train_and_evaluate.py
