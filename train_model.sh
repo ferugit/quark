77df1cf3049d9b536d8609b671e56ca4965033ee
ferugit <wiliam.lopezgavilanez@telefonica.com>
2023-03-02 16:58:35 +0100

    fix bash script

--- train_model.sh
+++ train_model.sh
@@ -2,16 +2,20 @@
 # Fernando López Gavilánez, 2023
 
 
+# Audio dataset path
+data_path='/home/wfl/Projects/donateacry-corpus/donateacry_corpus_cleaned_and_updated_data'
+partition_path='partitions/partition_20230302151711'
+
 # Set training parameters
+seed=2023
 optimizer='adam'
 momentum=0.9
 weight_decay=0.0
-seed=2023
 epochs=80
 batch_size=12
 patience=10
 lr=0.001
-cuda=True
+cuda=true
 balance=True
 # arch
 # augments
@@ -24,4 +28,8 @@ sampling_rate=16000
 num_classes=5
 
 
-python -u src/train_and_evaluate.py
+python -u src/train_and_evaluate.py --seed $seed --optimizer $optimizer \
+-- momentum $momentum --weigth_decay $weight_decay --epochs $epochs --batch_size $batch_size \
+--patience $patience --lr $lr --cuda $cuda --balance $balance --window_size $window_size \
+--sampling_rate $sampling_rate --num_classes $num_classes --data_path $data_path \
+--partition_path $partition_path
